1139e696db79e3a83fe87b80b117d415d960a8ff
Mr. E-Cyber Team <111108508+teamcyber-glitch@users.noreply.github.com>
2023-07-30 18:07:59 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-pkg update && pkg upgrade && apt-get update && apt-get upgrade && pkg install nmap traceroute whois git bash python curl wget figlet ruby nodejs ffmpeg neofetch mc nano python2 python3 -y
+pkg update && pkg upgrade && apt-get update && apt-get upgrade && pkg install php nmap traceroute whois git bash python curl wget figlet ruby nodejs ffmpeg neofetch mc nano python2 python3 -y
 pip install colorama
 pip install -r requirements.txt 
